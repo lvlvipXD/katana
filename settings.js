94e53e9524c76519d6468ca8017d894fff4ec3dd
lvlvipXD <106407846+lvlvipXD@users.noreply.github.com>
2022-07-18 18:25:35 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -42,7 +42,7 @@ global.mess = {
     group: 'Feature Used Only For Groups!',
     private: 'Features Used Only For Private Chat!',
     bot: 'This Feature Is Only For Bot',
-    wait: 'Wait A minute...',
+    wait: '*ثانية كدا...*',
     error: 'Error! Maybe Api Key Is Expired🤔!',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
 }
